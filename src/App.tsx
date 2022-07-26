ed3a65bc22fcaa9a64434a70383de59df1ed599c
dimas novan arif wicaksono <dimas.novan@sicepat.com>
2022-07-26 16:59:02 +0700

    test ajaib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React, { FC } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-const ExamplePage = React.lazy(
-  () => import(/* webpackChunkName: example-page */ "./Pages/ExamplePage")
-);
+const ExamplePage = React.lazy(() => import("./Pages/ExamplePage"));
 
 const App: FC = () => {
   return (
